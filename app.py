ab7745a60c7acdc49e9812aaad88669aed8d72c5
markko98 <pavliskomarko@gmail.com>
2021-02-25 13:34:50 +0100

    added new app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import jsonify
+
+app = Flask(__name__)
+
+books = [
+    {'name' : 'Snow White', 'author':'Grimm brothers'},
+    {'name' : "Alice's adventures in wonderland", 'author':'Lewis Carrol'},
+]
+
+
+
+@app.route("/", methods=['GET'])
+def hello_world():
+	return "Hello world"
+
+@app.route("/api/books", methods=['GET'])
+def return_all():
+    return jsonify({'books': books})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True);
