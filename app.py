ceaf7dd1767c3970555f021cf96fe86b7217891e
markko98 <pavliskomarko@gmail.com>
2021-02-25 13:01:27 +0100

    app.py return books added

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+books = [
+    {'name' : 'Snow White', 'author':'Grimm brothers'},
+    {'name' : "Alice's adventures in wonderland", 'author':'Lewis Carrol'},
+]
+
+
+
+@app.route("/", methods=['GET'])
 def hello_world():
 	return "Hello world"
+
+@app.route("api/books", methods=['GET'])
+def return_all():
+    return jsonify({'books': books})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80);
