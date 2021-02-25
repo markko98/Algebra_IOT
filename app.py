744de3612c06c95090b20ede10679cb9e39236b9
markko98 <pavliskomarko@gmail.com>
2021-02-25 13:56:42 +0100

    mysql setup

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask import jsonify
+from flask_mysqldb import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+
+#mySQL configuration
+app.config['MYSQL_USER'] = 'bookuser'
+app.config['MYSQL_PASSWORD'] = 'bookuser123'
+app.config['MYSQL_DB'] = 'booksDB'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql.init_app(app)
 
 books = [
     {'name' : 'Snow White', 'author':'Grimm brothers'},
@@ -16,6 +25,15 @@ def hello_world():
 
 @app.route("/api/books", methods=['GET'])
 def return_all():
+    return jsonify({'books': books})
+
+
+@app.route("/api/books/titles", methods=['GET'])
+def return_titles():
+    titles=[]
+    for book in books:
+        titles.append(book['name'])
+    
     return jsonify({'books': books})
 
 if __name__ == "__main__":
