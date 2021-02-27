9651023a4030d8d2ea7d1ed1688b896f6264d034
markko98 <42501650+markko98@users.noreply.github.com>
2021-02-27 20:46:58 +0100

    temp js

---
+++ static/js/chartTemperature.js
@@ -0,0 +1,39 @@
+$.ajax({
+    url: "http://algebra-iot-2021-00.westeurope.cloudapp.azure.com:80/api/telemetry/measurement",
+    data: {
+        "DeviceId": '1',
+        "SensorName": "Temperature", 
+        "dateFrom": "2021-02-01",
+        "dateTo": "2022-02-25"
+    },
+    cache: false,
+    type: "GET",
+    success: function (response) {
+        values = [];
+        captions = [];
+        for (var i = 0; i < response.length; i++) {
+            captions[i] = response[i]["CreatedOn"];
+            values[i] = response[i]["SensorValue"];
+        }
+ 
+        var ctx = document.getElementById("Mychart");
+        var mychart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: captions,
+                datasets: [
+                    {
+                        data: values,
+                        label: "Temperature",
+                        borderColor: "#3e95cd",
+                        fill: false
+                    },
+                ]
+            }
+        });
+    },
+    error: function (xhr) {
+        console.log("Ajax error!");
+        console.log(xhr);
+    }
+});
