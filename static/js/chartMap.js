a7349790f0bed2fc51843e3b24ea407f60d3a37b
markko98 <42501650+markko98@users.noreply.github.com>
2021-02-27 19:53:02 +0100

    frontend things ajax

--- static/js/chartMap.js
+++ static/js/chartMap.js
@@ -1,20 +1,24 @@
-$.ajax({url:"http://pavliskomiot2021.westeurope.cloudapp.azure.com:80/api/telemetry/devices",
-data:{
-},
-cache:false;
-type="GET",
-success: function(response) {
-	devids = [];
-	names =[];
-	lats = [];
-	longs =[];
-	for(var i = 0; i < response.length;i++){
-		devids[i] = response[i]['DeviceId'];
-		names[i] = response[i]['Name'];
-		lats[i] = response[i]['latitude'];
-		longs[i] = response[i]['longitude'];
-	}
-	var map = L.map('Mymap').setView([45.2705, 15.1500], 3);
+$.ajax({
+    url: "http://pavliskomiot2021.westeurope.cloudapp.azure.com:80/api/telemetry/devices",
+    data: {
+    },
+    cache: false,
+    type: "GET",
+    success: function (response) {
+    devids = [];
+    names = [];
+    lats = [];
+    longs = [];
+    for (var i = 0; i < response.length; i++)
+    {
+        devids[i] = response[i]['DeviceId'];
+        names[i] = response[i]['Name'];
+        lats[i] = response[i]['latitude'];
+        longs[i] = response[i]['longitude'];
+ 
+    }
+        
+    var map = L.map('Mymap').setView([45.2705, 15.1500], 3);
         
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
